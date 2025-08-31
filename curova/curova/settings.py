eacbaf097d75a7daa7c26e61d3c578f1cc9c97b5
Ogwaluonye Onyedika Tochukwu <51770535+ether2314@users.noreply.github.com>
2025-08-31 22:50:32 +0100

    Update settings.py

--- curova/curova/settings.py
+++ curova/curova/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'curova.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600,
@@ -130,7 +130,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
