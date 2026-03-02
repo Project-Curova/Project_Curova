c08b3dfc2ad777f41c1aadc8f62f2a7859522c68
Ogwaluonye Onyedika Tochukwu <51770535+ether2314@users.noreply.github.com>
2026-03-02 23:09:57 +0100

    Update build.sh

--- curova/build.sh
+++ curova/build.sh
@@ -9,7 +9,7 @@ echo "🔹 Running collectstatic..."
 python manage.py collectstatic --no-input
 
 echo "🔹 Running database migrations..."
-python manage.py migrate --fake-initial
+python manage.py migrate
 
 echo "🔹 Checking for Swagger compatibility..."
 # No extra install needed for swagger in prod, but ensures cache is cleared
